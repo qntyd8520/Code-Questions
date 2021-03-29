3ba2e1e4a2704240ed3cc9749a96a0a016809698
guileful <81571770+Guileful666@users.noreply.github.com>
2021-03-30 08:29:38 +0900

    Add files via upload

---
+++ Source code/Exam0101.java
@@ -0,0 +1,17 @@
+package Exam01;
+
+public class Exam0101 {
+
+	public static void main(String[] args) {
+		// int형 변수 year를 선언하고 자신이 태어난 년도를 대입합니다.
+		//age 변수를 선언하고, year 변수를 사용하여 자신의 나이를 계산하여 대입하세요.
+		
+		int year1 = 1994;
+		int year2 = 2021;
+		int age = year2-year1+1 ; //+1을 한 이유는 만으로 나이가 나오지 않게 하기 위해서.
+		
+		System.out.println(age +"세 입니다.");
+
+	}
+
+}
